46be81f6b0e54f38001c58c2add24bd065e29d75
SchmidtAlejo <schmidtalejo@gmail.com>
2023-06-06 21:30:34 -0300

    update link project

--- src/helpers/projects.js
+++ src/helpers/projects.js
@@ -2,7 +2,7 @@ import rmWebAppImage from "./../assets/rm-web-app.png";
 
 const RM_WEB_APP_NAME = "Rick-Morty-WebApp";
 const RM_WEB_APP_GIT_HUB = "https://github.com/SchmidtAlejo/React-Web-App";
-const RM_WEB_APP_WEB = "https://github.com/SchmidtAlejo/React-Web-App";
+const RM_WEB_APP_WEB = "https://schmidtalejo.github.io/Rick-Morty-WebApp/";
 
 export default [
   {
